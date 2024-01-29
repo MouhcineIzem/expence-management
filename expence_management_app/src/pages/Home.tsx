6ec08a172d1b6d4eeee24caf048566f5acde0845
Mouhcine Bannani <mouhcine.bannani@gmail.com>
2024-01-29 21:09:06 +0100

    Get all projects that exists in the back

--- expence_management_app/src/pages/Home.tsx
+++ expence_management_app/src/pages/Home.tsx
@@ -1,12 +1,26 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 
 
 function Home() {
     const [projectName, setProjectName] = useState('');
     const [participants, setParticipants] = useState([]);
+    const [projects, setProjects] = useState([]);
     const [results, setResults] = useState(null);
 
+    useEffect(() => {
+        getAllProjects();
+    }, []);
+
+    const getAllProjects = async () => {
+        try {
+            const response = await axios.get('http://localhost:3000/expense');
+            console.log(response.data);
+            setProjects(response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
     const createProject = async () => {
         try {
             const response = await axios.post('https://localhost:3000/expense/create-project', {
@@ -33,6 +47,22 @@ function Home() {
     return (
         <div className="App">
             <h1>Expense Management</h1>
+           <h2>All Projects:</h2>
+            <ul>
+            {projects.map((project) => (
+                <li key={project.id}>
+                project name :  { project.name}
+                    <ul>
+                        {project.users.map((user) => (
+                            <li key={user.name}>
+                                user name : {user.name} <br/>
+                                expense : {user.expense}
+                            </li>
+                            ))}
+                    </ul>
+                </li>
+            ))}
+            </ul>
             <label>
                 Project Name:
                 <input type="text" value={projectName} onChange={(e) => setProjectName(e.target.validationMessage)} />
